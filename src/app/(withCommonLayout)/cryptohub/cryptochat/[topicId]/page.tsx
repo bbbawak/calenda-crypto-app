5c3d9b37a3318f8f1ef5b306d2a8d1c883846814
BBBawak <BBBawak@protonmail.com>
2025-06-07 10:23:05 -0400

    Fix build issues and deploy successfully

--- src/app/(withCommonLayout)/cryptohub/cryptochat/[topicId]/page.tsx
+++ src/app/(withCommonLayout)/cryptohub/cryptochat/[topicId]/page.tsx
@@ -1,23 +1,7 @@
 "use client";
 
-import { useGetSingleTopicQuery } from "@/redux/features/api/topicApi";
-import { usePathname } from "next/navigation";
-import { setPaths } from "@/redux/features/slices/authSlice";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import TopicDetailsPage from "@/components/Cryptohub/TopicDetailsPage";
-import Loading from "@/components/Shared/Loading";
-
-export async function generateStaticParams() {
-  return [
-    { topicId: '1' },
-    { topicId: '2' },
-    { topicId: '3' },
-    { topicId: 'dummy-1' },
-    { topicId: 'dummy-2' },
-    { topicId: 'dummy-3' },
-    { topicId: 'dummy-4' }
-  ];
-}
+import TopicDetails from '@/components/Cryptohub/TopicDetails';
+import React from 'react'
 
 interface PageProps {
     params: {
@@ -28,7 +12,7 @@ interface PageProps {
 const page = ({ params: { topicId } }: PageProps) => {
     return (
         <div>
-            <TopicDetailsPage topicId={topicId} />
+            <TopicDetails topicId={topicId} />
         </div>
     )
 }
